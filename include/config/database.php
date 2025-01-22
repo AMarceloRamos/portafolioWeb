09e2b3d8e03707ec732896e4bc6c2fabe90f6396
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-01-22 20:19:50 -0300

    Update database.php
    actualizacion de msqli a pdo

--- include/config/database.php
+++ include/config/database.php
@@ -1,23 +1,14 @@
 <?php
+function conectarDB() {
+    $dsn = "pgsql:host=dpg-ctq6ofqj1k6c739pn1ng-a;port=5432;dbname=dbcontacto;";
+    $user = "dbcontacto_user";
+    $password = "NmmS5wypyPKhdSZPkQCnPk4hb6toH1SJ";
 
-
-function conectarDB() : Mysqli{
-
-    // cargando las credenciales desde las varaibles del entorno
-
-    $host =getenv('DB_HOST') ?: 'dpg-ctq6ofqj1k6c739pn1ng-a';
-    $user = getenv('DB_USER') ?: 'dbcontacto_user';
-    $password = getenv('DB_PASSWORD') ?: 'NmmS5wypyPKhdSZPkQCnPk4hb6toH1SJ';
-    $database = getenv('DB_NAME') ?: 'dbcontacto';
-
-    $db= new mysqli( $host, $user , $password ,  $database);
-
-    if(!$db){
-        die( "Error no se puede conectar". $db->connect_error);
-     
+    try {
+        $db = new PDO($dsn, $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    } catch (PDOException $e) {
+        die("Error al conectar a la base de datos: " . $e->getMessage());
     }
-
-    return $db;
-
-
 }
