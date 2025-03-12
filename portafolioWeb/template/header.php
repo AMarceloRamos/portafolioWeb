b69d27f6c693a263f685544df644f8bd0eeecfb7
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-12 15:50:48 -0300

    Update header.php

--- portafolioWeb/template/header.php
+++ portafolioWeb/template/header.php
@@ -17,7 +17,7 @@
 
     <!-- Custom Fonts -->
 
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lato:400,300,100,700,900' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
