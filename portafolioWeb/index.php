09df707cac2c45c07febeafc0acf14854c7c585e
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-13 16:27:21 -0300

    Update index.php

--- portafolioWeb/index.php
+++ portafolioWeb/index.php
@@ -8,7 +8,7 @@ include 'template/header.php';
                                 <div class="intro-text">
                                     <h1 class="intro-lead-in animated bounceInLeft">Adrian Ramos</h1>
                                     <h2 class="intro-heading animated bounceInRight">Developer</h2>
-                                    <p class="intro-paragraph animated bounceInRight">Bievenido a  mi sitio web dedicado al desarrollo web y al diseño</p>
+                                    <p class="intro-paragraph animated bounceInRight">Bienvenido a mi sitio web dedicado al desarrollo web y al diseño</p>
                                 </div>
                                 <a href="#services" class="page-scroll btn btn-xl slider-button animated bounceInUp">Acerca de mi</a>  
     </div>
