a0ffced24bec20515f82cddcccb5bcdbcaa666d7
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-13 19:25:37 -0300

    Update about.php

--- portafolioWeb/about.php
+++ portafolioWeb/about.php
@@ -19,7 +19,9 @@
                             <h3 class="team-member-name">Adrián Ramos</h3>
                             <p class="text-muted-role">Analista Programador</p>
                             <p class="team-text-short">Egresado de Técnico en Análisis y Programación Computacional, con certificaciones en Desarrollo Web y Técnico en Armado y Configuración de Computadoras.</p>
-                            <p class="team-text-short">Posee más de cuatro años de experiencia desarrollando sitios web y aplicaciones móviles, desde plataformas para marcas personales hasta soluciones de comercio electrónico. </p>
+                            <p class="team-text-short">Poseo más de cuatro años de experiencia en el desarrollo de sitios web y aplicaciones móviles, desde plataformas para marcas personales hasta soluciones de comercio electrónico. Mi formación en el Instituto IACC, junto con capacitaciones en desarrollo web frontend y backend, me ha permitido especializarme en tecnologías como Node.js, Ruby y NetBeans, además de bases de datos relacionales con PostgreSQL y herramientas como DBeaver para la gestión y optimización de consultas.
+
+Durante mi práctica profesional en Emasa en 2023 como analista programador, desarrollé un sistema web interno con autenticación, gestión de roles y acceso dinámico a bases de datos. Además, he trabajado en múltiples proyectos personales, aplicando soluciones integrales en desarrollo web y optimizando rendimiento, seguridad y escalabilidad mediante arquitecturas modernas y mejores prácticas de desarrollo. </p>
                             <ul class="list-inline social-buttons">
                                 <li><a href="https://www.linkedin.com/in/adri%C3%A1n-ramos-b3869447/" target="t_bank"><i class="fab fa-linkedin"></i></a>
                                 <li><a href="https://github.com/AMarceloRamos" target="t_bank"><i class="fab fa-github"></i></a></li>
