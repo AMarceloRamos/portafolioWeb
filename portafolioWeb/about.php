61bedff554bd2575fb67f75ab429ca099b92f5d7
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-13 00:26:25 -0300

    Update about.php

--- portafolioWeb/about.php
+++ portafolioWeb/about.php
@@ -25,11 +25,9 @@
                                 </li>
                                 <li><a href="#"><i class="fa fa-facebook"></i></a>
                                 </li> -->
-                                <li><a href="https://www.linkedin.com/in/adri%C3%A1n-ramos-b3869447/" target="t_bank"><i class="fa fa-linkedin"></i></a>
-                                <li><a href="https://github.com/AMarceloRamos" target="t_bank"><i class="fa fa-github"></i></a>
-                                </li> 
-                                <h3 class="section-subheading" > <a href="documents/curriculum.pdf" download="adrian_ramos_cv.pdfclass="page-scroll btn btn-primary  btn-xl slider-button animated bounceInUp">↓ Descargar <span style="font-size:12px"></span>Hoja de vida</a> 
-                                
+                                <li><a href="https://www.linkedin.com/in/adri%C3%A1n-ramos-b3869447/" target="t_bank"><i class="fab fa-linkedin"></i></a>
+                                <li><a href="https://github.com/AMarceloRamos" target="t_bank"><i class="fab fa-github"></i></a></li> 
+                                <h3 class="section-subheading" > <a href="documents/curriculum.pdf" download="adrian_ramos_cv.pdfclass="page-scroll btn btn-primary  btn-xl slider-button animated bounceInUp">↓ Descargar <span style="font-size:12px"></span>Hoja de vida</a>  
                             </ul>
                         </div>
                     <!-- </div> -->
