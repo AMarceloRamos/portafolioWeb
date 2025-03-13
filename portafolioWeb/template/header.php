07dd6760ff730cc1ac5167c37e8d50131d455abf
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-13 00:19:30 -0300

    Update header.php

--- portafolioWeb/template/header.php
+++ portafolioWeb/template/header.php
@@ -18,6 +18,8 @@
     <!-- Custom Fonts -->
 
     <link href="/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
     <link href='https://fonts.googleapis.com/css?family=Lato:400,300,100,700,900' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
