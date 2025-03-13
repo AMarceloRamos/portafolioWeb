9508b7d001b388fabbc252fb9a687e33367bdb28
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-13 12:56:25 -0300

    Update about.php

--- portafolioWeb/about.php
+++ portafolioWeb/about.php
@@ -23,7 +23,7 @@
                             <ul class="list-inline social-buttons">
                                 <li><a href="https://www.linkedin.com/in/adri%C3%A1n-ramos-b3869447/" target="t_bank"><i class="fab fa-linkedin"></i></a>
                                 <li><a href="https://github.com/AMarceloRamos" target="t_bank"><i class="fab fa-github"></i></a></li>
-                                 <li><a href="https://wa.me/996279786" target="t_bank"><i class="fab fa-whatsapp"></i></a></li>
+                                 <li><a href="https://wa.me/987829030" target="t_bank"><i class="fab fa-whatsapp"></i></a></li>
                                 <li><a href="https://www.tiktok.com/@adrianramosinfoma?_t=ZM-8udgqbDWgDv&_r=1https://github.com/AMarceloRamos" target="t_bank"><i class="fab fa-tiktok"></i></a></li> 
                                 <h3 class="section-subheading" > <a href="documents/curriculum.pdf" download="adrian_ramos_cv.pdfclass="page-scroll btn btn-primary  btn-xl slider-button animated bounceInUp">↓ Descargar <span style="font-size:12px"></span>Hoja de vida</a>  
                             </ul>
