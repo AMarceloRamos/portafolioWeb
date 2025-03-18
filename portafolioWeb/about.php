b553f042a85d88d04c3039cbe1c08c7c414ca92d
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-17 23:27:03 -0300

    Update about.php

--- portafolioWeb/about.php
+++ portafolioWeb/about.php
@@ -3,12 +3,8 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Acerca de Mi</h2>
-                    <!-- <h3 class="section-subheading text-muted">
-                    </h3> -->
                 </div>
             </div>
-            <!-- <div id="myCarousel-two" class="carousel slide"> -->
-                <!-- Wrapper for Slides -->
                 <div class="carousel-inner team-wrapper">
                     <div class="item active">
                         <!-- Team Member One -->
@@ -19,9 +15,13 @@
                             <h3 class="team-member-name">Adrián Ramos</h3>
                             <p class="text-muted-role">Analista Programador</p>
                             <p class="team-text-short">Egresado de Técnico en Análisis y Programación Computacional, con certificaciones en Desarrollo Web y Técnico en Armado y Configuración de Computadoras.</p>
-                            <p class="team-text-short">Poseo más de cuatro años de experiencia en el desarrollo de sitios web y aplicaciones móviles, desde plataformas para marcas personales hasta soluciones de comercio electrónico. Mi formación en el Instituto IACC, junto con capacitaciones en desarrollo web frontend y backend, me ha permitido especializarme en tecnologías como Node.js, Ruby y NetBeans, además de bases de datos relacionales con PostgreSQL y herramientas como DBeaver para la gestión y optimización de consultas.
-
-Durante mi práctica profesional en Emasa en 2023 como analista programador, desarrollé un sistema web interno con autenticación, gestión de roles y acceso dinámico a bases de datos. Además, he trabajado en múltiples proyectos personales, aplicando soluciones integrales en desarrollo web y optimizando rendimiento, seguridad y escalabilidad mediante arquitecturas modernas y mejores prácticas de desarrollo. </p>
+                            <p class="team-text-short">
+                                    Poseo más de cuatro años de experiencia en el desarrollo de sitios web y aplicaciones móviles, desde plataformas para marcas personales hasta soluciones de comercio electrónico. 
+                                    Mi formación en el Instituto IACC, junto con capacitaciones en desarrollo web frontend y backend, me ha permitido especializarme en tecnologías como Node.js, Ruby y NetBeans,
+                                    además de bases de datos relacionales con PostgreSQL y herramientas como DBeaver para la gestión y optimización de consultas.
+                                    Durante mi práctica profesional en Emasa en 2023 como analista programador, desarrollé un sistema web interno con autenticación, gestión de roles y acceso dinámico a bases de datos. Además, 
+                                    he trabajado en múltiples proyectos personales, aplicando soluciones integrales en desarrollo web y optimizando rendimiento, seguridad y escalabilidad mediante arquitecturas modernas y mejores prácticas de desarrollo.
+                            </p>
                             <ul class="list-inline social-buttons">
                                 <li><a href="https://www.linkedin.com/in/adri%C3%A1n-ramos-b3869447/" target="t_bank"><i class="fab fa-linkedin"></i></a>
                                 <li><a href="https://github.com/AMarceloRamos" target="t_bank"><i class="fab fa-github"></i></a></li>
@@ -29,23 +29,8 @@ Durante mi práctica profesional en Emasa en 2023 como analista programador, des
                                 <li><a href="https://www.tiktok.com/@adrianramosinfoma?_t=ZM-8udgqbDWgDv&_r=1https://github.com/AMarceloRamos" target="t_bank"><i class="fab fa-tiktok"></i></a></li> 
                                 <h3 class="section-subheading" > <a href="documents/curriculum.pdf" download="adrian_ramos_cv.pdfclass="page-scroll btn btn-primary  btn-xl slider-button animated bounceInUp">↓ Descargar <span style="font-size:12px"></span>Hoja de vida</a>  
                             </ul>
-                        </div>
-                    <!-- </div> -->
-                 
+                                </div>    
                 </div>
-                <!-- Controls -->
-                <!-- <a class="left carousel-control" href="#myCarousel-two" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                </a> -->
-                <!-- <a class="right carousel-control" href="#myCarousel-two" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a> -->
-                <!-- Indicators -->
-                <!-- <ol class="carousel-indicators">
-                    <li data-target="#myCarousel-two" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel-two" data-slide-to="1"></li>
-                    <li data-target="#myCarousel-two" data-slide-to="2"></li>
-                </ol> -->
             </div>
         </div>
     </section>
