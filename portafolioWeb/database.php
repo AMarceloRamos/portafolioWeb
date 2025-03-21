e7d8636698e004f560d6e959a78345d212b37579
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-21 17:29:16 -0300

    Update database.php

--- portafolioWeb/database.php
+++ portafolioWeb/database.php
@@ -5,10 +5,10 @@ function conectarDB() : Mysqli{
 
     // cargando las credenciales desde las varaibles del entorno
 
-    $host =getenv('DB_HOST') ?: 'dpg-ctq6ofqj1k6c739pn1ng-a';
-    $user = getenv('DB_USER') ?: 'dbcontacto_user';
-    $password = getenv('DB_PASSWORD') ?: 'NmmS5wypyPKhdSZPkQCnPk4hb6toH1SJ';
-    $database = getenv('DB_NAME') ?: 'dbcontacto';
+    $host =getenv('DB_HOST') ?: 'dpg-cves1qfnoe9s73b9ullg-a';
+    $user = getenv('DB_USER') ?: 'portafoliodb_hb96_user';
+    $password = getenv('DB_PASSWORD') ?: 'LUd1w30XSO8bNfXvwb5TwFrflfRnka8K';
+    $database = getenv('DB_NAME') ?: 'portafolioDB';
 
     $db= new mysqli( $host, $user , $password ,  $database);
 
