7bffc653e3219bcc3d5f5ee1acd8dedc2f23dd51
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-03-28 12:44:35 -0300

    Update conocimiento.php

--- portafolioWeb/conocimiento.php
+++ portafolioWeb/conocimiento.php
@@ -12,7 +12,6 @@
                 <div class="pricing text-center">
                     <div class="pricing-top-box">
                         <h3 class="pricing-title">Lenguajes</h3>
-                
                     </div>
                     <ul>
                         <li>Javascripts</li>
@@ -20,14 +19,14 @@
                         <li>Java</li>
                         <li>Ruby</li>
                     </ul>
-                    <a href="" class="btn btn-send">Sign Up</a>
+                     <h2>lenguajes</h2>
+                        <div class="chart-container" id="chartlenguajes"></div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="pricing text-center">
                     <div class="pricing-top-box">
                         <h3 class="pricing-title">Herramientas</h3>
-                        
                     </div>
                     <ul>
                         <li>Git</li>
@@ -36,24 +35,24 @@
                         <li>Postman</li>
                         <li>Dbeaver</li>
                     </ul>
-                    <a href="" class="btn btn-send">Sign Up</a>
+                    <h2>herramientas</h2>
+                        <div class="chart-container" id="chartHerramientas"></div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="pricing text-center">
                     <div class="pricing-top-box">
                         <h3 class="pricing-title">IDES/FrameWorks</h3>
-                        
                     </div>
                     <ul>
+                        <li>Angular</li>
+                        <li>React</li>
                         <li>Apache Netbeans</li>
                         <li>Android Studio</li>
-                        <li>NodeJs</li>
-                        <li>ruby and rails</li>
-                        <li>Express</li>
-                        <li>React</li>
+                        <li>ruby and rails</li>                   
                     </ul>
-                    <a href="" class="btn btn-send">Sign Up</a>
+                   <h2>IDES/Frameworks</h2>
+                        <div class="chart-container" id="chartFrameworks"></div>
                 </div>
             </div>
         </div>
